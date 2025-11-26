fbf2e5485f75d5a79bd78f261fa37473ef8c8744
IstarVin <ajdeleverio20@gmail.com>
2025-11-26 09:35:52 +0800

    feat: integrate Toaster component into layout for improved notifications

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { Toaster } from '$lib/components/ui/sonner';
+	import './layout.css';
 
 	let { children } = $props();
 </script>
@@ -10,3 +11,4 @@
 </svelte:head>
 
 {@render children()}
+<Toaster />
