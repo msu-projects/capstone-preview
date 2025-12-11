9d747d739092a85287311512616d77e473c0f3df
IstarVin <ajdeleverio20@gmail.com>
2025-12-11 21:17:45 +0800

    refactor: add AppBreadcrumb component to recommendations page

--- src/routes/(public)/recommendations/+page.svelte
+++ src/routes/(public)/recommendations/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+	import AppBreadcrumb from '$lib/components/AppBreadcrumb.svelte';
 	import SitioRecommendations from '$lib/components/public/SitioRecommendations.svelte';
 </script>
+
+<!-- Breadcrumb -->
+<AppBreadcrumb items={[{ label: 'Recommendations' }]} />
 
 <div class="container mx-auto p-6">
 	<SitioRecommendations isAdminView={false} />
